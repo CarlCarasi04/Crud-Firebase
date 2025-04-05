41482cfa09011312eeb2fb2d27ef64c831c5f870
CarlCarasi04 <carlcarasi0@gmail.com>
2025-04-05 18:30:49 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDAwfZnKkFq1_iQABu4OR-WIj6K69lpQ8k",
+  authDomain: "crud-9b59e.firebaseapp.com",
+  databaseURL: "https://crud-9b59e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-9b59e",
+  storageBucket: "crud-9b59e.firebasestorage.app",
+  messagingSenderId: "974887353325",
+  appId: "1:974887353325:web:26f565de0e2257062405e4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
